0913ad1fbd60b6636832df61d8fd72e7c254297d
laila-saw <81462565+laila-saw@users.noreply.github.com>
2021-12-27 19:50:38 +0100

    commit

---
+++ api/index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const app=express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute=require("./routes/users");
+const postRoute=require("./routes/posts");
+const authRoute=require("./routes/auth");
+const multer=require("multer");
+const path =require("path");
+const cors=require("cors");
+dotenv.config();
+ mongoose.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true},
+    () => {
+      console.log('Connected to MongoDB');
+    }
+  );
+  // app(cors());
+  const storage=multer.diskStorage({
+    destination : (req,file,cb)=>{
+      cb(null,"public/images")
+    },
+    filename:(req, file, cb)=>{
+      cb(null, file.originalname);
+    }
+  });
+  const upload=multer({storage:storage});
+  
+  app.post("/api/upload",upload.single("file"),(req,res)=>{
+    try{
+      return res.status(200).json("file uploaded succefly");
+    }catch(err){
+      res.status(500).json(err)
+      console.log(error)
+    }
+  });
+  app.use("/images", express.static(path.join(__dirname, "public/images")));
+// middleware 
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+app.use("/api/auth", authRoute)
+// app.length('*',()=>{
+// // send the index html from build folder 
+// });
+app.listen(8800,()=>{
+    console.log('hi laila! backend server is runing yyyyess ');
+})
